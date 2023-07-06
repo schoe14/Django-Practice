8a8878158489b625795de8516ea92327837b3376
Seohui Kim <chelsea.choe@gmail.com>
2023-07-06 21:43:18 +0900

    Added 404 error for non-existing question id

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import Http404
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
@@ -14,7 +14,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
