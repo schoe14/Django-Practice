46e658863a7a1e681b990acf3ed0a36b3f6610ef
Seohui Kim <chelsea.choe@gmail.com>
2023-07-31 20:38:25 +0900

    Fixed the admin page to display choices by registering Choice

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
 class QuestionAdmin(admin.ModelAdmin):
@@ -9,3 +9,4 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
