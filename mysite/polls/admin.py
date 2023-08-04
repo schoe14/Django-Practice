4c42dd702cf09ed92ed34c1d390eb51afe3169bd
Seohui Kim <chelsea.choe@gmail.com>
2023-08-04 19:15:46 +0900

    Added pub_date, was_published_recently, filter sideba and search capability to the admin change list

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -7,10 +7,9 @@ class ChoiceInline(admin.TabularInline):
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
-    ]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,5 @@
 # Create your models here.
-
+from django.contrib import admin
 import datetime
 from django.db import models
 from django.utils import timezone
@@ -12,6 +12,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
